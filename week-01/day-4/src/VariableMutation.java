2a64fb6cea4a7f58338e4be956bf9bebbd64e1d6
Laszlo Kereki <xaddew@gmail.com>
2019-09-12 15:53:42 +0200

    "VariableMutation.java added"

--- week-01/day-4/src/VariableMutation.java
+++ week-01/day-4/src/VariableMutation.java
@@ -3,24 +3,28 @@ public class VariableMutation {
         int a = 3;
         // make the "a" variable's value bigger by 10
         a = a + 10;
+        // Vagy a = += 10;
 
         System.out.println(a);
 
         int b = 100;
         // make b smaller by 7
         b = b - 7;
+        // Vagy b = -= 7;
 
         System.out.println(b);
 
         int c = 44;
         // please double c's value
         c = c * 2;
+        // Vagy c *= 2;
 
         System.out.println(c);
 
         int d = 125;
         // please divide by 5 d's value
         d = d / 5;
+        // Vagy d /= 5;
 
         System.out.println(d);
 
@@ -34,35 +38,42 @@ public class VariableMutation {
         int f1 = 123;
         int f2 = 345;
         // tell if f1 is bigger than f2 (print as a boolean)
-        if f1 > f2 {
-            System.out.println(boolean);
-        }
+        boolean f = f1 > f2;
+        System.out.println(f);
 
         int g1 = 350;
         int g2 = 200;
         // tell if the double of g2 is bigger than g1 (print as a boolean)
-        if (g2 * 2) > g1 {
-            System.out.println(boolean);
-        }
+        boolean g = g1 < (g2 * 2);
+        System.out.println(g);
 
         int h = 135798745;
         // tell if it has 11 as a divisor (print as a boolean)
-        if h % 11 = 0 {
-            System.out.println(boolean);
-        }
+        boolean feladatH = h % 11 == 0;
+            System.out.println(feladatH);
 
         int i1 = 10;
         int i2 = 3;
         // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
-        if Math.pow(i2, 3) > i1 > (i2 * i2) {
-            System.out.println(boolean);
-        }
+        boolean i = (Math.pow(i2, 3) > i1) && (i1 > (i2 * i2));
+        System.out.println(i);
 
         int j = 1521;
         // tell if j is dividable by 3 or 5 (print as a boolean)
-        if j % 3 = 0 || j % 5 = 0 {
-            System.out.println(boolean);
-        }
-        System.out.println(k);
+        boolean feladatJ = j % (3 * 5) == 0;
+        System.out.println(feladatJ);
+
+        /*Várt eredmények:
+        13
+        93
+        88
+        25
+        512
+        false
+        true
+        false
+        true
+        false*/
+
     }
 }

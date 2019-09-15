e0865eefd79ce7fceef6b26b34aaf550dc674f62
Laszlo Kereki <xaddew@gmail.com>
2019-09-15 22:51:09 +0200

    DrawChessTable added

---
+++ week-01/day-5/src/DrawChessTable.java
@@ -0,0 +1,24 @@
+public class DrawChessTable {
+    public static void main(String[] args) {
+        // Create a program that draws a chess table like this
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+        // % % % %
+        //  % % % %
+
+        String row1 = "  ██  ██  ██  ██";
+        String row2 = "██  ██  ██  ██  ";
+
+        for (int i = 1; i <= 8; i++) {
+            if (i % 2 == 0) {
+                System.out.println(row1);
+            } else {
+                System.out.println(row2);
+            }
+        }
+    }
+}

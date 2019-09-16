022ecdc687efdf13250669f7764ff00300501c0e
Laszlo Kereki <xaddew@gmail.com>
2019-09-16 20:39:18 +0200

    Factorio added, Sum typos fixed

---
+++ week-02/day-1/src/Factorio.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorio {
+    public static void main(String[] args) {
+        // - Create the usual class wrapper and main method on your own.
+        // - Create a function called `factorio` that returns it's input's factorial
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please add a number, I give you back its factorial: ");
+
+        int x = sum(input.nextInt());
+        System.out.println(x);
+    }
+
+    public static int sum(int y) {
+        int z = 1;
+        for (int i = 0; i < y; i++) {
+            z = z * (y - i);
+        }
+        return z;
+    }
+}
--- week-02/day-1/src/Sum.java
+++ week-02/day-1/src/Sum.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        // Create the usual class wrapper and main method on your own.
-        // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+        // - Create the usual class wrapper and main method on your own.
+        // - Write a function called `sum` that returns the sum of numbers from zero to the given parameter
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please add a number: ");
-                                            // Ha esetleg Dia és bajtársai ez elolvasnák... :)
+                                            // Ha esetleg Dia és bajtársai ezt elolvasnák... :)
         int x = sum(input.nextInt());       // 1. Inputból beveszi a sum(input) értékét, aminek az eredményére MAJD "x"-ként hivatkozik!
         System.out.println(x);              // 5. Kiírja az "x" értékét
     }

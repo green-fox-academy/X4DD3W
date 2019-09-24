f7081e7493b0cdf55354a64c4df267ab1856463b
Laszlo Kereki <xaddew@gmail.com>
2019-09-24 14:52:30 +0200

    ReversedOrder added

--- week-03/day-2/src/CountLines.java
+++ week-03/day-2/src/CountLines.java
@@ -6,10 +6,8 @@ import java.util.List;
 public class CountLines {
 
   public static void main(String[] args) {
-    // Write a function that takes a filename as string,
-    // then returns the number of lines the file contains.
-    // It should return zero if it can't open the file, and
-    // should not raise any error.
+    // Write a function that takes a filename as string, then returns the number of lines the file contains.
+    // It should return zero if it can't open the file, and should not raise any error.
 
     String zero = "my-file.txt";
     reader(zero);
@@ -22,7 +20,7 @@ public class CountLines {
       List<String> everyLine = Files.readAllLines(filepath);
       System.out.println(everyLine.size());
     } catch (Exception e) {
-      System.out.println("zero");
+      System.out.println(0);
     }
   }
 }
---
+++ week-03/day-2/src/ReversedOrder.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedOrder {
+
+  public static void main(String[] args) {
+    // Create a method that decrypts reversed-order.txt
+
+    String toDecrypt = "reversed-order.txt";
+    overWriter(toDecrypt);
+
+  }
+
+  private static void overWriter(String reversedText) {
+    try {
+      Path filepath = Paths.get(reversedText);
+      if (Files.exists(filepath)) {
+        List finalFile = decryptor(reversedText);
+        Files.write(filepath, finalFile);
+        System.out.println("Decryption was successful!");
+      }
+/*  } catch (StringIndexOutOfBoundsException a) {
+      System.out.println("String index out of range!");*/
+    } catch (IOException e) {
+      System.out.println("Unable to handle the file.");
+    }
+  }
+
+  private static List decryptor(String reversed) throws IOException {
+    List<String> zenOfPy = Files.readAllLines(Paths.get(reversed));
+    List<String> decryptedFile = new ArrayList<>();
+    String temp = "";
+
+    for (int i = zenOfPy.size() - 1; i > 0; i--) {
+      for (int j = 0; j < zenOfPy.get(i).length(); j++) {
+        temp = temp + zenOfPy.get(i).charAt(j);
+      }
+      decryptedFile.add(temp);
+      temp = "";
+    }
+    return decryptedFile;
+  }
+}

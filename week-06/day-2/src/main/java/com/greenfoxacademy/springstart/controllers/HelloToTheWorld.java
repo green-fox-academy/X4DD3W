0378bc21be7c1ecced7eb9ce1643be9dcc7a0a31
Laszlo Kereki <xaddew@gmail.com>
2019-10-23 21:47:27 +0200

    HelloToTheWorld updated

--- week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToTheWorld.java
+++ week-06/day-2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToTheWorld.java
@@ -1,9 +1,10 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.Random;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloToTheWorld {
@@ -15,12 +16,16 @@ public class HelloToTheWorld {
       "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
       "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
       "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+  private String[] colors = {"blue", "red", "purple", "grey", "yellow", "black", "green", "cyan"};
+  private Random random = new Random();
 
   @RequestMapping (value = "/helloToTheWorld")
-  public String greetingToTheWorld(@RequestParam String color, @RequestParam int fontsize, Model model) {
+  public String greetingToTheWorld(/*@RequestParam String color, @RequestParam int fontsize,*/ Model model) {
+    int fontsize = random.nextInt(200);
+    String color = colors[random.nextInt(colors.length)];
     model.addAttribute("colorToHTML", "color: " + color);
     model.addAttribute("sizeToHTML", "font-size: " + fontsize + "px");
-    model.addAttribute("languageToHTML", hellos[7]);
+    model.addAttribute("languageToHTML", hellos[random.nextInt(hellos.length)]);
     return "greetingToTheWorld";
   }
 

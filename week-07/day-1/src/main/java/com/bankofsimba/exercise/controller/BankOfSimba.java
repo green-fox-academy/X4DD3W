c302975b7095d3ec7a4ac149e9a8293e77ca1d2f
Laszlo Kereki <xaddew@gmail.com>
2019-10-28 15:07:42 +0100

    W07D01 task is in progress

---
+++ week-07/day-1/src/main/java/com/bankofsimba/exercise/controller/BankOfSimba.java
@@ -0,0 +1,34 @@
+package com.bankofsimba.exercise.controller;
+
+import com.bankofsimba.exercise.model.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BankOfSimba {
+  List<BankAccount> accounts = new ArrayList<>();
+
+  public BankOfSimba() {
+    accounts.add(new BankAccount("Simba", 2000, "lion", true, true));
+    accounts.add(new BankAccount("Nala", 1999, "lion"));
+    accounts.add(new BankAccount("Sarabi", 10000, "lion"));
+    accounts.add(new BankAccount("Zazu", 78, "bird"));
+    accounts.add(new BankAccount("Ed", -400, "hyena", false, false));
+  }
+
+  @GetMapping("/show")
+  public String fieldsOfBankAccount(Model model) {
+    model.addAttribute("accounts", accounts);
+    return "index";
+  }
+
+  @GetMapping("/text")
+  public String testHTML(Model model) {
+    model.addAttribute("text", "\"This is an <em>HTML</em> text. <b>Enjoy yourself!</b>\"");
+    return "text";
+  }
+
+}
---
+++ week-07/day-1/src/main/java/com/bankofsimba/exercise/model/BankAccount.java
@@ -0,0 +1,48 @@
+package com.bankofsimba.exercise.model;
+
+public class BankAccount {
+
+  private String name;
+  private double balance;
+  private String animalType;
+  private boolean isKing;
+  private boolean goodGuy;
+
+  public BankAccount(String name, double balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+    this.isKing = false;
+    this.goodGuy = true;
+  }
+
+  public BankAccount(String name, double balance, String animalType, boolean king, boolean goodGuy) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+    this.isKing = king;
+    this.goodGuy = goodGuy;
+  }
+
+  public BankAccount(){}
+
+  public String getName() {
+    return name;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public String getAnimalType() {
+    return animalType;
+  }
+
+  public boolean isKing() {
+    return isKing;
+  }
+
+  public boolean isGoodGuy() {
+    return goodGuy;
+  }
+}
